85c9694ed0c65b66e50c715870eab3451aaf4a0c
NekitaSokol <sleepingcat716@gmail.com>
2025-08-12 21:53:14 +0500

    tk3

--- 01.js
+++ 01.js
@@ -1,8 +1,6 @@
 // С помощью цикла while найдите сумму чисел от 1 до 20
 // Результат присвойте переменной sum
 
-// your code
-
 let sum = 0;  
 let number = 1;  
 while (number <= 20) {  
@@ -12,3 +10,5 @@ while (number <= 20) {
 }  
 
 console.log(sum);
+
+// your code
